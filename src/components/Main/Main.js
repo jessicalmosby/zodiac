9c9a04f3935c8e1cf4de8597c29b429d19b6ab93
Jessica Mosby <jessicamosby@mac.com>
2023-01-10 18:49:24 -0500

    added images

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,15 @@
+import React from 'react';
+import './Main';
+import Zodiac from '../Zodiac/Zodiac.js';
+import { zodiac } from '../../data.js';
 import background from '../../background.png';
 
 export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${background})` }}>
-      {/* {.map((item) => (
-        < key={item.name} {...item} />
-      ))} */}
-      <p>Hello World</p>
+      {zodiac.map((item) => (
+        <Zodiac key={item.name} {...item} />
+      ))}
     </main>
   );
 }
